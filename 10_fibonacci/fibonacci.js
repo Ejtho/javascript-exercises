1621abc9941e1ea900d07ea64483ab3775b3e2d2
Ejtho <89697386+Ejtho@users.noreply.github.com>
2023-02-13 15:55:30 +0000

    Add function to generate sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const sequence = [0, 1];
+    for (i = 2; i < num; i++) {
+        // Add the sum of the two previous numbers to the current index
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+    return sequence[num];
 };
 
 // Do not edit below this line
