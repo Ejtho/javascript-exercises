143766c6d1a36dcc6f2c79cb4be7deb2b1aa47e3
Ejtho <89697386+Ejtho@users.noreply.github.com>
2023-02-13 15:25:18 +0000

    Add function to check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // g to replace whitespace globally, then to lower case
+    let stripped = string.replace(/[^\w\']|_/g, "").toLowerCase();
+    let stringArray = Array.from(stripped);
+    if (stringArray.join("") == stringArray.reverse().join("")) {
+        return `${string} is a palindrome!`;
+    } else {
+        return `${string} is not a palindrome!`;
+    }
 };
 
 // Do not edit below this line
