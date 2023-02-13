0ab902acb3ff393dcb724aab34e29ec81f994dbd
Ejtho <89697386+Ejtho@users.noreply.github.com>
2023-02-13 12:23:54 +0000

    Add function to remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removal) {
+    const newArray = array;
+    if (typeof removal == "object") {
+        for (i = 0; i < array.length; i++) {
+            for (j = 0; j < removal.length; j++) {
+                if (Number(newArray[i]) == Number(removal[j])) {
+                    newArray.splice(i, 1);
+                }
+            }
+        } 
+    } else {
+        for (i= 0; i < array.length; i++) {
+            if (Number(newArray[i]) == Number(removal)) {
+                newArray.splice(i, 1);
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
