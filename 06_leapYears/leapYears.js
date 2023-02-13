31ce088b0a23ba555918384e40caa0808f2fcd58
Ejtho <89697386+Ejtho@users.noreply.github.com>
2023-02-13 12:59:42 +0000

    Add function to return whether a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                alert(`${year} is a leap year.`);
+            } else {
+                alert(`${year} is not leap year.`);
+            }
+        } else {
+            alert(`${year} is a leap year.`);
+        }
+    } else {
+        alert(`${year} is not leap year.`);
+    } 
 };
 
 // Do not edit below this line
